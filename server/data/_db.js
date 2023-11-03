0a811be2f6fe8cd549fb32715f1e590818c7021d
Rupam Datta <rupam@acceldata.io>
2023-11-03 14:50:31 +0530

    added gitignore file

---
+++ server/data/_db.js
@@ -0,0 +1,40 @@
+export const Users = [
+	{
+		"id": 1,
+		"name": 'John Doe',
+		"role": "CANDIDATE",
+		"address": {
+			"street": "",
+			"city": "",
+			"state": "",
+			"country": ""
+		}
+	}
+]
+
+export const Jobs = [
+	{
+		id: 1,
+		title: 'Title',
+		company: 'XYZ',
+		location: '',
+		salary: 1000.0,
+		description: 'lorem ipsum'
+	},
+	{
+		id: 2,
+		title: 'Title 2',
+		company: 'XYZ',
+		location: '',
+		salary: 1000.0,
+		description: 'lorem ipsum'
+	},
+	{
+		id: 3,
+		title: 'Title 3',
+		company: 'XYZ',
+		location: '',
+		salary: 1000.0,
+		description: 'lorem ipsum'
+	}
+]
